c779eb5a8ac6e55905035d31a7f51c4da792f4f3
Liu Xinan <xinan@me.com>
2015-08-03 23:12:37 +0800

    Add specs for `users/profile` endpoint

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,6 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  let(:sample_profile) do
+    {
+      nusnet_id: 'a0123456',
+      name: 'LIU XINAN',
+      email: 'xinan@u.nus.edu',
+      gender: 'Male',
+      faculty: 'School of Computing',
+      first_major: 'Computer Science (Hons)',
+      second_major: '',
+      matriculation_year: '2014',
+      ivle_token: 'this_is_a_token_if_you_believe'
+    }
+  end
+
+  let(:sample_output) do
+    {
+      userProfile: {
+        nusnetId: 'a0123456',
+        name: 'LIU XINAN',
+        email: 'xinan@u.nus.edu',
+        gender: 'Male',
+        faculty: 'School of Computing',
+        firstMajor: 'Computer Science (Hons)',
+        secondMajor: '',
+        matriculationYear: '2014',
+        accessToken: '6ppsSGwXujEDTc4V8IoOHRmR9/bWa4yfrs21ZccwT2Q='
+      }
+    }
+  end
 
   describe "GET #auth" do
     context 'when one or more of the required params are missing' do
@@ -17,20 +46,6 @@ RSpec.describe UsersController, type: :controller do
     end
 
     context 'when all required params are passed in' do
-      let(:sample_profile) do
-        {
-          nusnet_id: 'a0123456',
-          name: 'LIU XINAN',
-          email: 'xinan@u.nus.edu',
-          gender: 'Male',
-          faculty: 'School of Computing',
-          first_major: 'Computer Science (Hons)',
-          second_major: '',
-          matriculation_year: '2014',
-          ivle_token: 'this_is_a_token_if_you_believe'
-        }
-      end
-
       context 'when ivleToken is not valid or does not match with nusnetId' do
         it 'returns 401 unauthorized' do
           expect(IVLE).to receive_message_chain(:new, :get_profile).and_return(sample_profile)
@@ -43,29 +58,45 @@ RSpec.describe UsersController, type: :controller do
 
       context 'when ivleToken is valid and matches with nusnetId' do
         it 'returns 200 success along with user profile' do
-          expected = {
-            userProfile: {
-              nusnetId: 'a0123456',
-              name: 'LIU XINAN',
-              email: 'xinan@u.nus.edu',
-              gender: 'Male',
-              faculty: 'School of Computing',
-              firstMajor: 'Computer Science (Hons)',
-              secondMajor: '',
-              matriculationYear: '2014',
-              accessToken: '6ppsSGwXujEDTc4V8IoOHRmR9/bWa4yfrs21ZccwT2Q='
-            }
-          }
-
           expect(IVLE).to receive_message_chain(:new, :get_profile).and_return(sample_profile)
 
           get :auth, nusnetId: 'a0123456', ivleToken: 'this_is_a_token_if_you_believe'
 
           expect(response).to have_http_status(:success)
-          expect(response.body).to eq(expected.to_json)
+          expect(response.body).to eq(sample_output.to_json)
         end
       end
     end
   end
 
+  describe "GET #profile" do
+    context 'when user is not authenticated' do
+      it 'returns 401 unauthorized' do
+        expected = {
+          status: 401,
+          details: 'YOU SHALL NOT PASS!'
+        }
+
+        get :profile
+
+        expect(response).to have_http_status(:unauthorized)
+        expect(response.body).to eq(expected.to_json)
+      end
+    end
+
+    context 'when user is authenticated' do
+      it 'returns 200 success along with user profile' do
+        expect(User).to receive_message_chain(:where, :first)
+            .and_return(User.new(sample_profile.merge(access_token: '6ppsSGwXujEDTc4V8IoOHRmR9/bWa4yfrs21ZccwT2Q=')))
+
+        request.headers['nusnetId'] = 'a0123456'
+        request.headers['accessToken'] = 'this_is_a_token_if_you_believe'
+
+        get :profile
+
+        expect(response).to have_http_status(:success)
+        expect(response.body).to eq(sample_output.to_json)
+      end
+    end
+  end
 end
