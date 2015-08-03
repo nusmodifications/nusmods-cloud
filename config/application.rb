e4095c19a85da5af417141e9d3f76e1be517ef58
Liu Xinan <xinan@me.com>
2015-08-03 17:15:01 +0800

    Fix IVLE wrapper

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module NUSModsCloud
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Autoload libs
+    config.autoload_paths << Rails.root.join('lib')
   end
 end
--- lib/ivle.rb
+++ lib/ivle.rb
@@ -1,19 +1,39 @@
 class IVLE
   include HTTParty
-  base_uri 'ivle.nus.edu.sg/api/Lapi.svc'
+  base_uri 'https://ivle.nus.edu.sg/api/Lapi.svc'
 
-  def initialize(api_key, token)
+  def initialize(token, api_key = ENV['IVLE_API_KEY'])
     # Using both `AuthToken` and `Token` because IVLE auth params are inconsistent. (╯°□°)╯︵ ┻━┻
     @options = { query: { APIKey: api_key, AuthToken: token, Token: token } }
-    response = self.class.get('/Validate', @options)
-    if response.code != 200
-      raise 'Invalid API key!'
-    elsif response.parsed_response['Success'].blank?
-      raise 'Invalid token!'
+    begin
+      response = self.class.get('/Validate', @options)
+      if response.code != 200
+        raise 'Invalid API key!'
+      elsif response.parsed_response['Success'].blank?
+        raise 'Invalid token!'
+      end
+      @valid = true
+    rescue => e
+      Rails.logger.info { "[IVLE] Validation failed. Error: #{e}" }
+      @valid = false
     end
   end
 
-  def getProfile
+  ### get_profile | Get the student's profile from IVLE
+  # Sample result:
+  # {
+  #   nusnet_id: 'a0123456',
+  #   name: 'LIU XINAN',
+  #   email: 'xinan@u.nus.edu',
+  #   gender: 'Male',
+  #   faculty: 'School of Computing',
+  #   first_major: 'Computer Science (Hons)',
+  #   second_major: '',
+  #   matriculation_year: 2014
+  #   ivle_token: 'blah_blah_blah'
+  # }
+  def get_profile
+    return nil unless @valid
     begin
       response = self.class.get('/Profile_View', @options)
       result = response.parsed_response['Results'][0]
