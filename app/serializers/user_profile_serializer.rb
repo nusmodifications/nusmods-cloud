01b9d5b441fefc5eccda12e9ceba4c64911ea50c
Liu Xinan <xinan@me.com>
2016-01-09 15:00:46 +0800

    Include timetables in user profile payload

--- app/serializers/user_profile_serializer.rb
+++ app/serializers/user_profile_serializer.rb
@@ -1,3 +1,4 @@
 class UserProfileSerializer < ActiveModel::Serializer
   attributes :nusnet_id, :name, :email, :gender, :faculty, :first_major, :second_major, :matriculation_year, :access_token
+  has_many :timetables, serializer: TimetableSerializer
 end
