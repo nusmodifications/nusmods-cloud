79d0dd47d34c9746190bbb0de1144c4d66482fed
Liu Xinan <xinan@me.com>
2016-01-09 16:09:33 +0800

    Update `users_controllers` specs

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -27,41 +27,37 @@ RSpec.describe UsersController, type: :controller do
         firstMajor: 'Computer Science (Hons)',
         secondMajor: '',
         matriculationYear: '2014',
-        accessToken: '6ppsSGwXujEDTc4V8IoOHRmR9/bWa4yfrs21ZccwT2Q='
+        accessToken: 'bSR4ncNwDGms74xXe5RF3H7rY6XIb0GT',
+        timetables: []
       }
     }.to_json
   end
 
-  describe 'POST #auth' do
+  describe 'POST #create' do
     context 'when one or more of the required params are missing' do
       it 'returns 400 bad request' do
         post :create
-        expect(response).to have_http_status(:bad_request)
-
-        post :create, nusnetId: 'A0123456'
-        expect(response).to have_http_status(:bad_request)
-
-        post :create, ivleToken: 'this_is_a_token_if_you_believe'
         expect(response).to have_http_status(:bad_request)
       end
     end
 
     context 'when all required params are passed in' do
-      context 'when ivleToken is not valid or does not match with nusnetId' do
+      context 'when ivleToken is not valid' do
         it 'returns 401 unauthorized' do
-          expect(IVLE).to receive_message_chain(:new, :get_profile).and_return(sample_profile)
+          expect(IVLE).to receive_message_chain(:new, :get_profile).and_return(nil)
 
-          post :create, nusnetId: 'a6543210', ivleToken: 'this_is_a_token_if_you_believe'
+          post :create, ivleToken: 'this_is_a_token_if_you_believe'
 
           expect(response).to have_http_status(:unauthorized)
         end
       end
 
-      context 'when ivleToken is valid and matches with nusnetId' do
+      context 'when ivleToken is valid' do
         it 'returns 200 success along with user profile' do
           expect(IVLE).to receive_message_chain(:new, :get_profile).and_return(sample_profile)
+          expect(SecureRandom).to receive(:base64).with(24).and_return("bSR4ncNwDGms74xXe5RF3H7rY6XIb0GT")
 
-          post :create, nusnetId: 'a0123456', ivleToken: 'this_is_a_token_if_you_believe'
+          post :create, ivleToken: 'this_is_a_token_if_you_believe'
 
           expect(response).to have_http_status(:success)
           expect(response.body).to eq(sample_output)
@@ -91,7 +87,7 @@ RSpec.describe UsersController, type: :controller do
     context 'when user is authenticated' do
       it 'returns 200 success along with user profile' do
         expect(User).to receive_message_chain(:where, :first)
-            .and_return(User.new(sample_profile.merge(access_token: '6ppsSGwXujEDTc4V8IoOHRmR9/bWa4yfrs21ZccwT2Q=')))
+            .and_return(User.new(sample_profile.merge(access_token: 'bSR4ncNwDGms74xXe5RF3H7rY6XIb0GT')))
 
         request.headers['Authorization'] = 'this_is_a_token_if_you_believe'
 
