f4d50f1bb62063e3a447876968c82b2f07c8a94e
Liu Xinan <xinan@me.com>
2016-01-15 01:56:27 +0800

    Tweak CORS headers to only allow from `nusmods.com` and `staging.nusmods.com`

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module NUSModsCloud
     # Set rack-cors configs
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
-        origins '*'
+        origins 'https://nusmods.com', 'http://staging.nusmods.com'
         resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
       end
     end
